25045cdef2dc2a9b2ced2b793b16af42e0b299d9
Slobodan Dan <danastroman@gmail.com>
2025-02-22 21:18:10 +0100

    Added collections to db context and created initial db migration.

--- API/Program.cs
+++ API/Program.cs
@@ -14,4 +14,15 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try {
+    var context = services.GetRequiredService<ApiDbContext>();
+    await context.Database.MigrateAsync();
+} catch (Exception ex) {
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+}
+
 app.Run();
--- Persistence/ApiDbContext.cs
+++ Persistence/ApiDbContext.cs
@@ -1,7 +1,11 @@
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
 public class ApiDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<SoftwareService> SoftwareServices { get; set; }
 }
