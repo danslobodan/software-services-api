6821bc82d9b31c5d4424c186fa4f1ae440b4a03a
Slobodan Dan <danastroman@gmail.com>
2025-02-28 14:46:00 +0100

    Fixed db initialization - posgres required UTC datee time format.

--- Application/Software/DTOs/OrderSoftwareRequest.cs
+++ Application/Software/DTOs/OrderSoftwareRequest.cs
@@ -4,5 +4,5 @@ public class OrderSoftwareRequest
 {
     public required string Id { get; set; }
     public required int Quantity { get; set; }
-    public required int DurationMonths { get; set; }
+    public required DateTime ValidTo { get; set; }
 }
--- Application/SoftwareLicenses/Validators/PurchaseSoftwareLicenseValidator.cs
+++ Application/SoftwareLicenses/Validators/PurchaseSoftwareLicenseValidator.cs
@@ -9,9 +9,9 @@ public class PurchaseSoftwareLicenseValidator : AbstractValidator<PurchaseSoftwa
     {
         RuleFor(x => x.Dto.AccountId)
             .NotEmpty().WithMessage("AccountId is required.");
-        RuleFor(x => x.Dto.DurationMonths)
-            .NotEmpty().WithMessage("Duration is required.")
-            .GreaterThan(0).WithMessage("Number of months must be a positive number.");
+        RuleFor(x => x.Dto.ValidTo)
+            .NotEmpty().WithMessage("ValidTo is required.")
+            .GreaterThan(DateTime.Now).WithMessage("ValidTo date must be in the future");
         RuleFor(x => x.Dto.Name)
             .NotEmpty().WithMessage("Name is required.");
         RuleFor(x => x.Dto.Quantity)
